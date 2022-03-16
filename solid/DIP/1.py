83fa24a03bd70334de74907ffcc620f166558c31
Khanze99 <khamidov_anvar@mail.ru>
2022-03-16 11:53:20 +0300

    description

--- solid/DIP/1.py
+++ solid/DIP/1.py
@@ -1,30 +1,42 @@
+"""
+Application использует кокнретную реализацию Concreteimpl через интерфейс Service.
+Однако приложению требуется каким-то образом создавать экземпляры ConcreteImpl.
+Чтобы решить эту задачу бещ образования зависимости от ConcreteImpl на уровне исходного кода,
+приложение вызывает метод make_svc интерфейса фабрики ServiceFactory. Этот  метод реализован в классе
+ServiceFactoryImpl, наследующем ServiceFactory. Эта реализация создает экземпляр ConcreteImpl и возвращает его
+как экземпляр интерфейса Service
+"""
+
+
 from abc import abstractmethod
 
 
 class IService:
-
-    @abstractmethod
-    def makeSvc(self):
-        pass
-
-
-class IServiceFactory(IService):
-    def makeSvc(self):
-        return ConcreteImpl()
-
-
-class ServiceFactoryImpl(IServiceFactory):
     pass
 
 
-class ConcreteImpl:
+class IServiceFactory(IService):
+    @abstractmethod
+    def make_svc(self):
+        pass
+
+
+class ServiceFactoryImpl(IServiceFactory):
+    def make_svc(self):
+        return ConcreteImpl()
+
+
+class ConcreteImpl(IService):
     pass
 
 
 class Application:
 
-    def __init__(self, service: ServiceFactoryImpl):
-        self.service = service
+    def __init__(self, concrete_impl: ConcreteImpl):
+        self.concrete_impl = concrete_impl
 
-    def some_behavior(self):
-        concrete_impl = self.service.makeSvc()
+
+if __name__ == '__main__':
+    service_factory = ServiceFactoryImpl()
+    concrete_impl = service_factory.make_svc()
+    app = Application(concrete_impl)
